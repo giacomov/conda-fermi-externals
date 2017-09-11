8ae68394b8fcca93bf28a2d816be8fdff432279d
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-09-11 15:26:27 -0700

    OSX build

--- build_inside_container.sh
+++ build_inside_container.sh
@@ -1,5 +1,14 @@
 # Get miniconda
-wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O ~/miniconda.sh
+if [ "$(uname)" == "Darwin" ]; then
+
+    wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O ~/miniconda.sh
+
+else
+    
+    wget -q https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+    
+fi
+
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
@@ -7,17 +16,30 @@ conda install -y conda-build
 conda install -y anaconda-client
 conda update -y conda conda-build anaconda-client
 
-# Use this if you want to upload to the channel
-conda config --set anaconda_upload yes
-
-# Some package need zip
-yum install -y zip
-
+# Activate environment
 source /root/miniconda/bin/activate
 
 # Copy .tar if any
 mkdir -p /root/miniconda/conda-bld/src_cache
 cp /conda-fermi-externals/*.tar* /root/miniconda/conda-bld/src_cache
 
-conda build -c fermi_dev_externals ${MY_CONDA_PACKAGE}
+if [ -z ${CI+x} ]; then
+
+    # We are in Travis CI
+    # Use this if you want to upload to the channel
+
+    conda config --set anaconda_upload no
+
+    conda build ${MY_CONDA_PACKAGE}
+
+else
+
+    # Use this if you want to upload to the channel
+
+    conda config --set anaconda_upload yes
+
+    conda build -c fermi_dev_externals ${MY_CONDA_PACKAGE}
+
+fi
+
 
