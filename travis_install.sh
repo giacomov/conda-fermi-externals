27679a79e754810f381585606db554ff2bfb1ffe
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-09-11 17:06:07 -0700

    Caching tar file from heasoft

--- travis_install.sh
+++ travis_install.sh
@@ -9,6 +9,10 @@ else
     # Install some custom requirements on Linux
     
     sudo apt-get -qq update
-    sudo apt-get install -y zip
+    sudo apt-get install -y zip curl
     
 fi
+
+# Now download the .tar file from heasoft so we do not download it 3 times
+sudo mkdir -p /conda-fermi-externals
+curl "https://heasarc.nasa.gov/cgi-bin/Tools/tarit/tarit.pl?mode=download&arch=src&src_pc_linux_sci=Y&src_other_specify=&general=futils" -o /conda-fermi-externals/heasoft-6.22src.tar.gz
