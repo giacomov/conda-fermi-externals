b64779e26a4b18e8550cf3b380adc4419f1cddb9
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-09-12 17:01:24 -0700

    A test script to test some Bash functionality

---
+++ test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+python -c "import time;time.sleep(3);1/1" && exit 0 || exit 10 &
+
+export pid=$!
+
+while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    
+    echo "Still building"
+    
+    sleep 1
+
+done
+
+wait $pid
+
+exit_status=$?
+
+echo $exit_status
+
+if [[ $exit_status -eq 0 ]]; then
+
+    echo "good"
+    
+else
+
+    echo "bad"
+
+fi
